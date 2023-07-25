0083f8cfca1c49cff3b34252ceb81cd91dd96e7d
Ha Cao <xuanhavcdn@gmail.com>
2023-07-25 17:03:33 +0700

    Update baseUrl and fix element locator

--- cypress/e2e/pages/loginPage.ts
+++ cypress/e2e/pages/loginPage.ts
@@ -1,9 +1,9 @@
 const LoginPage = {
     acceptIntroductionBtn: "button[class = 'MuiButtonBase-root MuiButton-root MuiButton-text styles_btn__7t7Xt styles_fill_btn___gIs2']",
-    loginOptionBtn: "button[class='MuiButtonBase-root MuiButton-root MuiButton-text jss10 jss14 styles_buttonStyle__VxlRq']",
+    loginOptionBtn: "button[class='MuiButtonBase-root MuiButton-root MuiButton-text styles_btn_signUp__Z35Tu']",
     userName: "input[placeholder='Nhập số điện thoại hoặc email']",
     passWord: "input[placeholder='Nhập mật khẩu']",
-    loginBtn: "button[type='submit']",
+    loginBtn: "button[type='submit'][data-test = 'signin-submit']",
     loginBtn_Xpath: "//button[@type='submit']",
     myAccountIcon: "[href='/my-account']"
 }
